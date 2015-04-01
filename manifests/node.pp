059962b1d9e575ac7aec294461022b7d3128ed11
Frank Groeneveld <frank@ivaldi.nl>
2015-04-01 14:02:12 +0200

    Only create sudoers changes for non-root. Make sure homedir exists as well.

--- manifests/node.pp
+++ manifests/node.pp
@@ -9,19 +9,37 @@ class rsnapshot::node($to_location, $ensure=present, $user='root') {
     user        => $user,
   }
 
-  user { $user:
-    ensure     => $ensure,
-    managehome => true,
-  }
 
+  if $user != 'root' {
+
+    user { $user:
+      ensure     => $ensure,
+    }
+
+    file { "/home/$user":
+      ensure     => 'directory',
+      mode       => '0755',
+      owner      => $user,
+      group      => $user,
+      require    => User[$user],
+    }
+
+    file_line { "$user sudo rsync":
+      ensure => $ensure,
+      path   => '/etc/sudoers',
+      line   => "$user ALL= NOPASSWD: /usr/bin/rsync --server",
+    }
+
+  } else {
+
+    user { $user:
+      ensure => present,
+    }
+
+  }
   Rsnapshot::Node::Master_definition <<| location_name == $to_location |>> {
     user    => $user,
     require => User[$user],
   }
 
-  file_line { "$user sudo rsync":
-    ensure => $ensure,
-    path   => '/etc/sudoers',
-    line   => "$user ALL= NOPASSWD: /usr/bin/rsync --server",
-  }
 }
