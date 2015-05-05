1a316322770a54679ec8111ced923ace9791ca0a
Bruno Léon <bruno.leon@unyonsys.com>
2015-05-05 13:12:30 +0200

    fix (node_definition): ensure backup is always treated as an array even wih only one element

--- manifests/master/node_definition.pp
+++ manifests/master/node_definition.pp
@@ -3,9 +3,9 @@ define rsnapshot::master::node_definition(
   $to_location,
   $identifier,
   $ipaddress,
-  $backup = {},
+  $backup = [],
 ) {
-
+  $backup_array = any2array($backup)
   concat::fragment { "rsnapshot_node_${name}":
     target  => $rsnapshot::params::config_path,
     content => template('rsnapshot/rsnapshot_node.conf.erb'),
